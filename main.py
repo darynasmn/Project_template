9cc6b8de530ea68f44ddbb15eafc97d574e885b6
Daryna Semenets <daryna.smn@gmail.com>
2024-03-17 21:20:23 +0100

    Test input and output functions due to files in the data folder

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ def main():
 
     text_from_console = input_console()
 
-    content_from_builtin = read_builtin("file.txt")
+    content_from_builtin = read_builtin("data/test.txt")
 
-    data_from_pandas = read_pandas("file.csv")
+    data_from_pandas = read_pandas("data/test_csv.csv")
     all_texts = [text_from_console, content_from_builtin, str(data_from_pandas)]
-
+    all_texts_str = '\n'.join(map(str, all_texts))
+    
     print_to_console(all_texts)
-    write_to_file("output.txt", all_texts)
+    write_to_file("data/output.txt", all_texts_str)
     
 
 if __name__ == "__main__":
