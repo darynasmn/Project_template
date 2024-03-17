69f0f2434d83bfbbb81c487aa6464a888c967959
Daryna Semenets <daryna.smn@gmail.com>
2024-03-17 18:28:56 +0100

    Empty output functions with docstings

--- app/io/output.py
+++ app/io/output.py
@@ -1,5 +1,32 @@
 def print_to_console(text):
+    """
+    Function to print text to the console.
+
+    Args:
+        text (str): The text to be printed to the console.
+
+    Examples:
+        >>> print_to_console('Hello, world!')
+        Hello, world!
+
+    Raises:
+        Exception: If an error occurs during printing.
+    """
     pass
 
 def write_to_file_with_builtin(file_path, content):
+    """
+    Function to write content to a file using Python's built-in capabilities.
+
+    Args:
+        file_path (str): The path to the file where content will be written.
+        content (str): The content to be written to the file.
+
+    Examples:
+        >>> write_to_file_with_builtin('example.txt', 'Content to be written.')
+        'Write operation successful.'
+
+    Raises:
+        Exception: If an error occurs during file writing.
+    """
     pass
