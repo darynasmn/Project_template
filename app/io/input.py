5f2a5bd72149da4117a358ac307a535000ec185b
Daryna Semenets <daryna.smn@gmail.com>
2024-03-17 21:19:24 +0100

    Implemented input functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def input_text_from_console():
     """
     Function to input text from the console.
@@ -5,15 +6,14 @@ def input_text_from_console():
     Returns:
         str: Text input by the user.
 
-    Examples:
-        >>> input_text_from_console()
-        Enter text: Hello, world!
-        'Hello, world!'
-
     Raises:
         Exception: If an error occurs during input.
     """
-    pass
+    try:
+        text = input("Enter text: ")
+        return text
+    except Exception as e:
+        raise Exception("An error occurred during input.") from e
 
 def read_from_file_with_builtin(file_path):
     """
@@ -25,15 +25,18 @@ def read_from_file_with_builtin(file_path):
     Returns:
         str: Content read from the file.
 
-    Examples:
-        >>> read_from_file_with_builtin('example.txt')
-        'Content of the file.'
-
     Raises:
         FileNotFoundError: If the file specified by file_path does not exist.
         Exception: If an error occurs during file reading.
     """
-    pass
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found at path: {file_path}") from e
+    except Exception as e:
+        raise Exception(f"An error occurred during file reading: {e}") from e
 
 def read_from_file_with_pandas(file_path):
     """
@@ -45,12 +48,14 @@ def read_from_file_with_pandas(file_path):
     Returns:
         DataFrame: Data read from the file using pandas.
 
-    Examples:
-        >>> read_from_file_with_pandas('data.csv')
-        pandas DataFrame containing data from the file.
-
     Raises:
         FileNotFoundError: If the file specified by file_path does not exist.
         Exception: If an error occurs during file reading.
     """
-    pass
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"File not found at path: {file_path}") from e
+    except Exception as e:
+        raise Exception(f"An error occurred during file reading: {e}") from e
